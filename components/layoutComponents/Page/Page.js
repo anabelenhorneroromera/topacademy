d27ddf953607ab59664f8b144f06493aac50addc
Alejandro Sánchez Serna <Alejadro.SanchezSerna@UGent.be>
2025-12-10 13:02:21 +0100

    Portapapeles

--- components/layoutComponents/Page/Page.js
+++ components/layoutComponents/Page/Page.js
@@ -1,25 +1,27 @@
 import { Component } from "react";
 import Headermenu from "../../genericComponents/Headermenu/Headermenu";
-// import MainFooter from "./specificComponents/MainFooter/MainFooter"; 
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-
 
 export default class Page extends Component {
   constructor(props) {
     super(props);
   }
 
-
   render() {
+    const blok = this.props.blok;
+
     return (
       <>
-        <main {...storyblokEditable(this.props.blok)}>
-          <Headermenu blok={this.props.menu.content}></Headermenu>
-          {this.props.blok.body.map((nestedBlok) => (
-            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-          ))}
+        <main {...storyblokEditable(blok)}>
+          <Headermenu blok={this.props.menu?.content} />
+
+          {/* ✔ Evita el crash si body no existe o no es array */}
+          {Array.isArray(blok.body) &&
+            blok.body.map((nestedBlok) => (
+              <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+            ))
+          }
         </main>
-        {/* <MainFooter />  */}
       </>
     );
   }
